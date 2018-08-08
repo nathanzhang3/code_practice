4608de48db7f85673d27058e3b5b8aa880ed82a7
Nathan <zhanghengzhezy@gmail.com>
2018-08-07 21:04:19 -0500

    1. rename functions 2. relocate csv data directory 3. build logic for updating orderbook

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import pymysql
 import os
+import csv
 
 HOST = "carloudydbtest.cqofkkppbnkd.us-east-2.rds.amazonaws.com"
 PORT = 3306
@@ -21,9 +22,10 @@ class DataBase(object):
         self.uname = uname
         self.pw = pw
         self.sym = symbol
+        self.order_book = {}
 
     def initialize_trade_csv(self):
-        self.trade_fname = 'bitfinex_trades_'+self.sym+'.csv'
+        self.trade_fname = 'data/bitfinex_trades_'+self.sym+'.csv'
 
         # Remove previous files
         try:
@@ -32,12 +34,12 @@ class DataBase(object):
             pass
 
         # Create csv file to store trades data
-        with open(trade_fname, 'a') as file:
+        with open(self.trade_fname, 'a') as file:
             writer = csv.writer(file, delimiter = ',')
             writer.writerow(['ID', 'Timestamp', 'Amount', 'Price'])
 
     def initialize_quote_csv(self):
-        self.quote_fname = 'bitfinex_quotes_'+self.sym+'.csv'
+        self.quote_fname = 'data/bitfinex_quotes_'+self.sym+'.csv'
 
         # Remove previous files
         try:
@@ -46,7 +48,7 @@ class DataBase(object):
             pass
 
         # Create csv file to store orders data
-        with open(quote_fname, 'a') as file:
+        with open(self.quote_fname, 'a') as file:
             writer = csv.writer(file, delimiter = ',')
             writer.writerow(['Price', 'Count', 'Amount'])
 
@@ -60,24 +62,60 @@ class DataBase(object):
         # Create a cursor object
         self.cur = self.conn.cursor()
 
-    def create_order_book_csv(self, snapshot):
+    def create_quote_csv(self, snapshot):
         for row in snapshot[0][0]:
-            with open('streaming_orders_'+self.symbol+'.csv', 'a') as file:
+            self.order_book[row[0]] = row[1:]
+            with open('data/bitfinex_quotes_'+self.sym+'.csv', 'a') as file:
                 writer = csv.writer(file, delimiter = ',')
                 writer.writerow(row)
+        print(self.order_book)
 
-    def create_sql_db(self, ):
-        try:
-            # Execute the sqlQuery
-            self.cur.execute('SHOW DATABASES')
+    def create_sql_db(self):
+        # Execute the sqlQuery
+        self.cur.execute('SHOW DATABASES')
 
-            #Fetch all the rows
-            databaseList = self.cur.fetchall()
+        #Fetch all the rows
+        databaseList = self.cur.fetchall()
 
-            if (dbname, ) in databaseList:
-                # Execute the create database SQL statment through the cursor
-                # instance
-                self.cur.execute('CREATE DATABASE' + dbname)
-                print('Database created.')
-            else:
-                print('Database exists.')
+        if (dbname, ) in databaseList:
+            # Execute the create database SQL statment through the cursor
+            # instance
+            self.cur.execute('CREATE DATABASE' + dbname)
+            #print('Database created.')
+        else:
+            print('Database exists.')
+
+    def update_trade_csv(self, new_trade):
+        if new_trade[0][0] == 'te': # keep only real-time data
+            with open('data/bitfinex_trades_'+self.sym+'.csv', 'a') as file:
+                writer = csv.writer(file, delimiter = ',')
+                writer.writerow(new_trade[0][1])
+
+    def update_quote_csv(self, quote_chg):
+        price = quote_chg[0]
+        count = quote_chg[1]
+        amount = quote_chg[2]
+
+        if count > 0:
+            if amount > 0:
+                if price in self.order_book:
+                    pass
+                else:
+                    pass
+            elif amount < 0:
+                if price in self.order_book:
+                    pass
+                else:
+                    pass
+        elif count == 0:
+            if amount == 1:
+                pass
+            elif amount == -1:
+                pass
+
+        os.remove(self.quote_fname)
+
+        for key, value in self.order_book.items():
+            with open('data/bitfinex_quotes_'+self.sym+'.csv', 'a') as file:
+                writer = csv.writer(file, delimiter = ',')
+                writer.writerow([key, value[0], value[1]])
