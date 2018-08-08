92c25550a5089125bb6e7d46f9879e505df2c425
Nathan <zhanghengzhezy@gmail.com>
2018-08-07 21:03:07 -0500

    implement message

---
+++ message.py
@@ -0,0 +1,21 @@
+import plivo
+
+AUTH_ID = 'MAZTQ5MZHLYMIXZDG2NT'
+AUTH_TOKEN = 'YjhlZWU1ZjEzZmM2OTcyM2MwZjhjNzQ5ZWYxY2Fk'
+SOURCE_ADDRESS = '11111111111'
+DESTINATION_NUMBER = '6149065034'
+def main():
+    client = plivo.RestClient(auth_id=AUTH_ID, auth_token=AUTH_TOKEN)
+    try:
+        response = client.messages.create(
+            src=SOURCE_ADDRESS,
+            dst=DESTINATION_NUMBER,
+            text='Server connection is disrupted!',
+        )
+        print(response.__dict__)
+    except plivo.exceptions.PlivoRestError as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    main()
